d4c6c9f9764b47094dcbceeba651d96c6b978592
Arafat Khan <arafat33k@outlook.com>
2020-09-01 00:26:22 +0600

    Create source code directory

---
+++ ForestKnight/constants.py
@@ -0,0 +1,7 @@
+""" Python file containing all the constants used throughout the game - all in one file """
+
+# Game window dimensions and title
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 650
+GAME_TITLE = "Forest Knight"
+
