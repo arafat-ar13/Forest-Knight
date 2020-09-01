4f38a299ebfe362d0933702739300a8c3b4a8837
Arafat Khan <arafat33k@outlook.com>
2020-09-01 19:46:10 +0600

    Basic level and player movement code implemented

---
+++ ForestKnight/game.py
@@ -0,0 +1,140 @@
+import os
+
+import arcade
+
+from characters.player.knight import Knight
+from constants import *
+from level_loader import level_loader
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+os.chdir(BASE_DIR)
+
+
+class GameWindow(arcade.Window):
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, GAME_TITLE)
+
+        arcade.set_background_color(arcade.csscolor.GREEN)
+
+        # Player sprite
+        self.player_list = None
+        self.player = None
+
+        # Other sprites
+        self.platforms = None
+        self.foregrounds = None
+        self.backgrounds = None
+        self.ladders = None
+        self.dont_touch = None
+
+        # Used to keep track of our scrolling
+        self.view_bottom = 0
+        self.view_left = 0
+
+        # Our simple Physics Engine
+        self.physics_engine = None
+
+    def setup(self, level):
+        """ 
+        Method that takes a level number and tries to load it
+        This method also is used for setting up the game quickly and efficiently
+        """
+
+        # Setting up Player sprite
+        self.player_list = arcade.SpriteList()
+        self.player = Knight()
+        self.player_list.append(self.player)
+
+        # Setting up other sprites
+        loaded_sprites = level_loader(level)
+
+        self.platforms = loaded_sprites[0]
+        self.foregrounds = loaded_sprites[1]
+        self.backgrounds = loaded_sprites[2]
+        self.ladders = loaded_sprites[3]
+        self.dont_touch = loaded_sprites[4]
+
+        # Setting up the physics engine
+        self.physics_engine = arcade.PhysicsEnginePlatformer(
+            self.player, self.platforms, gravity_constant=GRAVITY, ladders=self.ladders)
+
+    def on_draw(self):
+        arcade.start_render()
+
+        self.platforms.draw()
+        self.backgrounds.draw()
+        self.ladders.draw()
+        self.player_list.draw()
+        self.dont_touch.draw()
+        self.foregrounds.draw()
+
+    def on_key_press(self, key, modifiers):
+        """ Controls player movement when a key is pressed """
+        if key == arcade.key.W or key == arcade.key.UP:
+            if self.physics_engine.can_jump():
+                self.player.change_y = KNIGHT_JUMP_SPEED
+            elif self.physics_engine.is_on_ladder():
+                self.player.change_y = KNIGHT_SPEED
+        elif key == arcade.key.S or key == arcade.key.DOWN:
+            if self.physics_engine.is_on_ladder():
+                self.player.change_y = -KNIGHT_SPEED
+        elif key == arcade.key.D or key == arcade.key.RIGHT:
+            self.player.change_x = KNIGHT_SPEED
+        elif key == arcade.key.A or key == arcade.key.LEFT:
+            self.player.change_x = -KNIGHT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        """ Stops the player's movement when a key is released """
+        if key == arcade.key.W or key == arcade.key.UP:
+            self.player.change_y = 0
+        elif key == arcade.key.S or key == arcade.key.DOWN:
+            self.player.change_y = 0
+        elif key == arcade.key.D or key == arcade.key.RIGHT:
+            self.player.change_x = 0
+        elif key == arcade.key.A or key == arcade.key.LEFT:
+            self.player.change_x = 0
+
+    def on_update(self, dt):
+        """ Contains all the game loop and logic code """
+        self.physics_engine.update()
+
+        # --- Manage Scrolling ---
+
+        # Track if we need to change the viewport
+        changed = False
+
+        # Scroll left
+        left_boundary = self.view_left + LEFT_VIEWPORT_MARGIN
+        if self.player.left < left_boundary:
+            self.view_left -= left_boundary - self.player.left
+            changed = True
+
+        # Scroll right
+        right_boundary = self.view_left + SCREEN_WIDTH - RIGHT_VIEWPORT_MARGIN
+        if self.player.right > right_boundary:
+            self.view_left += self.player.right - right_boundary
+            changed = True
+
+        # Scroll up
+        top_boundary = self.view_bottom + SCREEN_HEIGHT - TOP_VIEWPORT_MARGIN
+        if self.player.top > top_boundary:
+            self.view_bottom += self.player.top - top_boundary
+            changed = True
+
+        # Scroll down
+        bottom_boundary = self.view_bottom + BOTTOM_VIEWPORT_MARGIN
+        if self.player.bottom < bottom_boundary:
+            self.view_bottom -= bottom_boundary - self.player.bottom
+            changed = True
+
+        if changed:
+            # Only scroll to integers. Otherwise we end up with pixels that
+            # don't line up on the screen
+            self.view_bottom = int(self.view_bottom)
+            self.view_left = int(self.view_left)
+
+            # Do the scrolling
+            arcade.set_viewport(self.view_left,
+                                SCREEN_WIDTH + self.view_left,
+                                self.view_bottom,
+                                SCREEN_HEIGHT + self.view_bottom)
--- ForestKnight/main.py
+++ ForestKnight/main.py
@@ -1,29 +1,13 @@
 import arcade
 
-from constants import *
-
-
-class GameWindow(arcade.Window):
-    def __init__(self):
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, GAME_TITLE)
-
-        arcade.set_background_color(arcade.csscolor.ORANGE_RED)
-
-    def setup(self):
-        pass
-
-    def on_draw(self):
-        arcade.start_render()
-
-    def on_update(self, dt):
-        pass
-
+from game import GameWindow
 
 def main():
     game = GameWindow()
-    game.setup()
+    game.setup(1)
 
     arcade.run()
+
 
 if __name__ == "__main__":
     main()
