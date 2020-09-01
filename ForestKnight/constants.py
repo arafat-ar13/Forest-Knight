c3084d5442119fca35ff76fd29081606820b9f04
Arafat Khan <arafat33k@outlook.com>
2020-09-01 19:45:37 +0600

    More constants

--- ForestKnight/constants.py
+++ ForestKnight/constants.py
@@ -5,3 +5,21 @@ SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 650
 GAME_TITLE = "Forest Knight"
 
+# Scaling
+TILE_SCALE = 0.5
+CHARACTER_SCALE = 0.2
+
+# Player scrolling
+# How many pixels to keep as a minimum margin between the character
+# and the edge of the screen.
+LEFT_VIEWPORT_MARGIN = 250
+RIGHT_VIEWPORT_MARGIN = 250
+BOTTOM_VIEWPORT_MARGIN = 50
+TOP_VIEWPORT_MARGIN = 100
+
+# Gravity
+GRAVITY = 1
+
+# Knight moving velocity and other constants
+KNIGHT_SPEED = 7
+KNIGHT_JUMP_SPEED = 17
